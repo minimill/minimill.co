1b88e294d5cbf691c3ce007106fe39baa3663b02
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-08-22 00:54:00 -0700

    more form code

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -12,20 +12,50 @@ function shouldShowSubmitButton() {
 
 function onInputChanged(e) {
     if (shouldShowSubmitButton()) {
-        submitButton.className += ' active';
+        console.log('yes');
+        submitButton.classList.add('active');
     } else {
-        submitButton.className.replace(' active', '');
+        console.log('no');
+        submitButton.classList.remove('active')
+        console.log(submitButton.className);
     }
 }
 
+function submitForm(e) {
+    e.preventDefault();
 
+    var xmlhttp= window.XMLHttpRequest ?
+        new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var response = JSON.parse(xmlhttp.responseText);
+            console.log(response);
+        } else {
+            var response = JSON.parse(xmlhttp.responseText);
+            console.log('error: ', response);
+        }
+    }
+
+    var name = document.getElementById('name').innerHTML;
+    var email = document.getElementById('email').innerHTML;
+    var message = document.getElementById('message').innerHTML;
+
+    // xmlhttp.open("GET","your_url.php?name=" + name + "&email=" + email, true);
+    // xmlhttp.open("POST","your_url.php",true);
+    // xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    // xmlhttp.send("name=" + name + "&email=" + email);
+}
 
 window.onload = function() {
-    inputs = document.querySelectorAll('.field input');
+    var form = document.getElementById('contact-form')[0];
+    inputs = document.querySelectorAll('.field input, .field textarea');
     for (var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
         input.addEventListener('input', onInputChanged);
     }
     submitButton = document.getElementById('submit-button');
     submitInput = document.getElementById('submit');
+    submitButton.addEventListener('click', submitForm);
+    form.addEventListener('submit', submitForm);
 }
