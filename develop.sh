0032404946349d601220f10ab848120205949dbc
Jeff Hilnbrand <jahilnbrand@gmail.com>
2015-08-21 23:47:29 -0700

    not good practice

--- develop.sh
+++ develop.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-virtualenv --no-site-packages .
-source bin/activate
-pip install -r config/requirements.txt
+virtualenv --no-site-packages /tmp/venv
+source /tmp/venv/bin/activate
+/tmp/venv/bin/pip install -r config/requirements.txt
